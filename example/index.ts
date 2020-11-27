9a4b40a41e3aeacf16aed3c4a564f3c80bf0711a
zesagata <zezagata17@gmail.com>
2020-11-27 10:25:08 +0700

    update example at typescript script

--- example/index.ts
+++ example/index.ts
@@ -2,16 +2,20 @@ import Klasifikasi from '../dist'
 
 (async () => {
 
-  await Klasifikasi.build({
-    creds: [
-      {
-        clientId: 'e249df6c-a9ef-4cc3-9f97-bf7f51426f2c',
-        clientSecret: 'e28c2b9c-ef77-4995-968c-285dae95ebab'
-      }
-    ]
-  })
-
-  const result = await Klasifikasi.classify('1j2318uakls', 'Menonton video ini membuat saya dan ibu saya menangis karena teringat akan jasa para pahlawan.')
-  console.log(result)
+  try {
+    await Klasifikasi.build({
+      creds: [
+        {
+          clientId: 'e249df6c-a9ef-4cc3-9f97-bf7f51426f2c',
+          clientSecret: 'e28c2b9c-ef77-4995-968c-285dae95ebab'
+        }
+      ]
+    })
+  
+    const result = await Klasifikasi.classify('1j2318uakls', 'Menonton video ini membuat saya dan ibu saya menangis karena teringat akan jasa para pahlawan.')
+    console.log(result)
+  } catch (error) {
+    console.log(error)
+  }
 
 })()
